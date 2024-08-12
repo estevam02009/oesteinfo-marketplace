12311e8114ed6ae51c0319b8b6cf94ec4a88146c
Manoel Estevam Neto <104177851+estevam02009@users.noreply.github.com>
2024-08-12 00:59:23 -0300

    Dashboard e BreadCrumb(iniciado)
    Nesta parte do projeto foi iniciado a implementação da Dashboard e de navegação através de bread-crumb.

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'image',
+    ];
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\ProductDetails;
+use App\Livewire\AdminDashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/product/details', ProductDetails::class);
+Route::get('/admin/dashboard', AdminDashboard::class)->middleware('admin');
